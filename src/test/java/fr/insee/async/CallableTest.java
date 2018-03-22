acbc956464dd6554ace12df640fbe5e852d48a3e
Romain Warnan <romain.warnan@insee.fr>
2018-03-22 11:26:49 +0100

    Callable is Done

--- src/test/java/fr/insee/async/CallableTest.java
+++ src/test/java/fr/insee/async/CallableTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
@@ -25,6 +26,24 @@ public class CallableTest {
 		
 		System.out.println("Exécution d'un calcul");
 		calculeService.longCalcul();
+		
+		System.out.println("Récupération du contenu du future");
+		Etablissement etablissement = future.get();
+		System.out.println(etablissement);
+	}
+	
+	@Test
+	public void callableIsDoneTest() throws InterruptedException, ExecutionException {
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		
+		System.out.println("Soumission du callable");
+		Future<Etablissement> future = executorService.submit(() -> sireneService.fetchOne(1000));
+		executorService.shutdown();
+		
+		while(!future.isDone()) {
+			System.out.println("Requête en cours...");
+			TimeUnit.MILLISECONDS.sleep(200);
+		}
 		
 		System.out.println("Récupération du contenu du future");
 		Etablissement etablissement = future.get();
