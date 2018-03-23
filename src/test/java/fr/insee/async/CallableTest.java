7aa6c1383620229e813a99fb7d04631a4a9bb6ab
Romain Warnan <romain.warnan@insee.fr>
2018-03-23 14:48:11 +0100

    Code produit durant le coding-dojo

--- src/test/java/fr/insee/async/CallableTest.java
+++ src/test/java/fr/insee/async/CallableTest.java
@@ -1,13 +1,16 @@
 package fr.insee.async;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -21,15 +24,24 @@ public class CallableTest {
 	
 	@Test
 	public void callable() throws InterruptedException, ExecutionException {
-		// Exécuter la fonction fetchOne()
-		// Lancer la fonction longCalcul() sans attendre le résultat de la fonction fetchOne.
-		// Imprimer le résultat de la fonction fetchOne
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		Future<Etablissement> future = executor.submit(() -> sireneService.fetchOne(1000));
+		calculeService.longCalcul();
+		Etablissement etablissement = future.get();
+		System.out.println(etablissement);
 	}
 	
 	@Test
 	public void callableIsDone() throws InterruptedException, ExecutionException {
 		// Exécuter la fonction fetchOne()
 		// Tant que le résultat n'est pas arivé, imprimer un message chaque 20 millisecondes.
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		Future<Etablissement> future = executor.submit(() -> sireneService.fetchOne(1000));
+		while(!future.isDone()){
+			System.out.println("en attente");
+			TimeUnit.MILLISECONDS.sleep(200);
+		}
+		System.out.println(future.get());
 		// Imprimer le résultat de la fonction fetchOne
 	}
 	
@@ -37,10 +49,30 @@ public class CallableTest {
 	public void invokeAll() throws InterruptedException, ExecutionException {
 		// Exécuter en parallèle 5 fois la fonction fetchOne en faisant varier le paramètre rank.
 		// Essayer de faire varier la taille du pool de threads
+		ExecutorService executor = Executors.newFixedThreadPool(5);
+		List<Integer> rank = Arrays.asList(10000,11000,12000,13000,14000);
+		List<Future<Etablissement>> listeFuture = executor.invokeAll(
+				rank.stream().map(r -> (Callable<Etablissement>) () -> sireneService.fetchOne(r))
+				.collect(Collectors.toList()));
+		for (Future<Etablissement> future : listeFuture) {
+			System.out.println(future.get());
+		}
+	
 	}
 	
 	@Test
 	public void invokeAny() throws InterruptedException, ExecutionException {
+		ExecutorService executor = Executors.newFixedThreadPool(5);
+		List<Callable<Etablissement>> callables = Arrays.asList(
+				() -> sireneService.fetchOne(10),
+				() -> sireneService.fetchOne(11000),
+				() -> sireneService.fetchOne(45),
+				() -> sireneService.fetchOne(13000),
+				() -> sireneService.fetchOne(14000));
+		Etablissement etb = executor.invokeAny(callables);
+		System.out.println(etb);
+
+		
 		// Exécuter en parallèle 5 fois la fonction fetchOne en faisant varier le paramètre rank.
 		// Imprimer le premier établissement à être retourné
 	}
--- src/test/java/fr/insee/async/CompletableFutureTest.java
+++ src/test/java/fr/insee/async/CompletableFutureTest.java
@@ -1,36 +1,43 @@
 package fr.insee.async;
 
-import static java.util.concurrent.CompletableFuture.allOf;
-import static java.util.concurrent.CompletableFuture.anyOf;
-import static java.util.concurrent.CompletableFuture.runAsync;
-import static java.util.concurrent.CompletableFuture.supplyAsync;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
 
-import org.assertj.core.util.Objects;
 import org.junit.Test;
 
 import fr.insee.async.sirene.service.CalculService;
 import fr.insee.async.sirene.service.SireneService;
 
 public class CompletableFutureTest {
-	
+
 	private SireneService sireneService = SireneService.getInstance();
 	private CalculService calculService = CalculService.getInstance();
-	
+
 	@Test
 	public void thenAccept() throws InterruptedException, ExecutionException {
+		// Run a task specified by a Supplier object asynchronously
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		CompletableFuture.supplyAsync(() -> sireneService.fetchOne(1000), executor)
+				.thenAccept(calculService::longCalcul)
+				.thenRun(()-> System.out.println("Terminé"));
+		calculService.longCalcul();
+		//pour attendre la fin du test
+		executor.shutdown();//n'accepte pas de nouvelles tâches. à faire à chaque fois
+		executor.awaitTermination(1, TimeUnit.MILLISECONDS);
+//		future.get();
 		// Exécuter la fonction fetchOne
-		// Une fois la fontion terminée, exécuter la fonction longCalcul avec en paramètre l'établissement retourné
+		// Une fois la fontion terminée, exécuter la fonction longCalcul avec en
+		// paramètre l'établissement retourné
 		// Imprimer "Terminé" dans la console
-		// En parallèle de ces opérations, exécuter la fonction longCalcul sans paramètre
+		// En parallèle de ces opérations, exécuter la fonction longCalcul sans
+		// paramètre
 	}
-	
+
 	@Test
 	public void thenApply() throws InterruptedException, ExecutionException, TimeoutException {
 		// Exécuter la fonction fetchFirst
@@ -38,16 +45,17 @@ public class CompletableFutureTest {
 		// Récupérer le premier établissement de la deuxième page
 		// En parallèle, exécuter la fonction longCalcul sans paramètre
 	}
-	
+
 	@Test
 	public void allOfTest() throws InterruptedException, ExecutionException, TimeoutException {
 		// Exécuter la fonction fetchOne et imprimer le résultat une fois obtenu
 		// En parallèle, lancer la fonction longCalcul
 	}
-	
+
 	@Test
 	public void anyOfTest() throws InterruptedException, ExecutionException, TimeoutException {
-		// Exécuter en parallèle 4 fois la fonction fetchOne en faisant varier le paramètre rank.
+		// Exécuter en parallèle 4 fois la fonction fetchOne en faisant varier
+		// le paramètre rank.
 		// Imprimer le premier établissement à être retourné
 	}
 }
